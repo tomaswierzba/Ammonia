0a1affa5887fb49c8cacaea89b2dd27d2e4c792c
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2023-02-15 16:59:46 +0100

    Update OnlineAmmoniaTool.py

--- OnlineAmmoniaTool.py
+++ OnlineAmmoniaTool.py
@@ -313,6 +313,7 @@ fig = go.Figure(go.Waterfall(
     measure = ["relative", "relative", "relative", "relative", "relative", "relative", "total"],
     x = ["Electricity","CAPEX Ammonia Synthesis","CAPEX Electrolyzer", "O&M Ammonia Synthesis", "Stack Replacement","O&M Electrolyzer", "LCoA"],
     textposition = "outside",
+    textsize=20,
     text = ["%s €/ton" % (LCoH_electricity_cost2), "%s €/ton" % (LCoH_capex_am_2),"%s €/ton" % (LCoH_capex_elec_2),"%s €/ton" % (LCoH_opex_ammonia2),"%s €/ton" % (LCoH_stack_rep_cost2),"%s €/ton" % (LCoH_opex_electrolyser2),"%s €/ton" % (LCoH2)],
     y = [LCoH_electricity_cost2,LCoH_capex_am_2,LCoH_capex_elec_2,LCoH_opex_ammonia2,LCoH_stack_rep_cost2, LCoH_opex_electrolyser2,LCoH2 ],
     connector = {"line":{"color":"rgb(63, 63, 63)"}},
