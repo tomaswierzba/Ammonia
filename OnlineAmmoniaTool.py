5725141079ea085f2688a4e8e76e683637535849
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2023-02-15 15:45:17 +0100

    Update OnlineAmmoniaTool.py

--- OnlineAmmoniaTool.py
+++ OnlineAmmoniaTool.py
@@ -304,7 +304,24 @@ else:
     st.altair_chart(rp, use_container_width=True)    
     
 
-    
+
+
+fig = go.Figure(go.Waterfall(
+    name = "20", orientation = "v",
+    measure = ["relative", "relative", "total", "relative", "relative", "total"],
+    x = ["Sales", "Consulting", "Net revenue", "Purchases", "Other expenses", "Profit before tax"],
+    textposition = "outside",
+    text = ["+60", "+80", "", "-40", "-20", "Total"],
+    y = [60, 80, 0, -40, -20, 0],
+    connector = {"line":{"color":"rgb(63, 63, 63)"}},
+))
+
+fig.update_layout(
+        title = "Profit and loss statement 2018",
+        showlegend = True
+)
+
+fig.show()    
     
     
     
