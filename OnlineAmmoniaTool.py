d911e193f55517c7b7f521a5d4dc6684f0a11a59
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2023-02-15 15:49:43 +0100

    Update OnlineAmmoniaTool.py

--- OnlineAmmoniaTool.py
+++ OnlineAmmoniaTool.py
@@ -13,7 +13,7 @@ import numpy as np
 import numpy_financial as npf
 import math
 import altair as alt
-import plotly.graph_objects as go
+
 
 
 #from IPython.display import Image
@@ -306,22 +306,7 @@ else:
 
 
 
-fig = go.Figure(go.Waterfall(
-    name = "20", orientation = "v",
-    measure = ["relative", "relative", "total", "relative", "relative", "total"],
-    x = ["Sales", "Consulting", "Net revenue", "Purchases", "Other expenses", "Profit before tax"],
-    textposition = "outside",
-    text = ["+60", "+80", "", "-40", "-20", "Total"],
-    y = [60, 80, 0, -40, -20, 0],
-    connector = {"line":{"color":"rgb(63, 63, 63)"}},
-))
-
-fig.update_layout(
-        title = "Profit and loss statement 2018",
-        showlegend = True
-)
-
-fig.show()    
+ 
     
     
     
